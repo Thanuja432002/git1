f651aef510c749667019de75741aeecac0949acb
Thanuja432002 <thanujareddy0403@gmail.com>
2023-08-08 08:01:04 +0530

    abcd

---
+++ linux-shell.sh
@@ -0,0 +1,13 @@
+mkdir oracledba123
+cd oracledba123
+touch da1  dd dc df dg dj
+cd ..
+touch af1 ag1 ch1
+git status
+ls -l
+git add  af1 ag1 ch1
+git status
+git commit -m "abbre"   af1 ag1 ch1
+git show
+git status
+
